77018006f3cd05fea7d21fab66d1116d3d012d9b
Tamara Robichet <tamara@heetch.com>
2022-06-23 15:51:20 +0200

    Add emoji search

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,11 @@
-// /api/create
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.post('/api/create', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- api/lib.tsx
+++ api/lib.tsx
@@ -1,32 +1,110 @@
-import { renderToFile } from "@react-pdf/renderer"
+import { renderToFile } from "@react-pdf/renderer";
+import React from "react";
+import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import Fuse from "fuse.js";
+import emoji from "@stdlib/datasets-emoji";
+
+const fuzzySearch = new Fuse(emoji(), {
+  keys: ["name", "group", "short_name", "description", "aliases", "keywords"],
+  includeMatches: true,
+  ignoreLocation: true,
+});
+
+const getEmoji = (label: string) => {
+  const icon: any = fuzzySearch.search(label.split(/\d+/)?.[1]?.trim())?.[0]
+    ?.item;
+
+  return icon?.emoji;
+};
 
 type RawRecipe = {
-  ingredients: string
-  instructions: string
-}
+  ingredients: string;
+  instructions: string;
+  title: string;
+};
 
 type Ingredient = {
-  quantity: string
-  description: string
-}
+  label: string;
+  icon: string;
+};
 
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+  },
+  section: {
+    margin: 20,
+    padding: 0,
+    flexGrow: 1,
+  },
+});
+
+const extractIngredient = (rawIngredient: string): Ingredient => {
+  return {
+    icon: getEmoji(rawIngredient || ""),
+    label: rawIngredient,
+  };
+};
 
 const formatIngredients = (rawIngredients: string): Ingredient[] => {
-  return []
-}
+  return rawIngredients
+    .split("\n")
+    .map((line: string) => extractIngredient(line));
+};
 
 const formatInstructions = (instructions: string): string[] => {
-  return []
-}
+  return [];
+};
 
-const FormattedRecipe = (ingredients: Ingredient[], instructions: string[]) => {
-  return <div>Formatted PDF</div>
-}
+const FormattedRecipe = (props: {
+  ingredients?: Ingredient[];
+  instructions?: string[];
+}) => {
+  console.log(props.ingredients);
+  return (
+    <div>
+      {" "}
+      <h1>Formatted PDF</h1>
+      <h2>Ingredients</h2>
+      <ul>
+        {props.ingredients.map((ingredient) => (
+          <li key={ingredient.label}>
+            <Text>
+              {ingredient.icon} {ingredient.label}
+            </Text>
+            <br />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export const generatePDF = async (rawRecipe: RawRecipe) => {
   const ingredients = formatIngredients(rawRecipe.ingredients);
   const instructions = formatInstructions(rawRecipe.instructions);
 
-  return renderToFile(<div />, `${process.cwd()}/my-doc.pdf`);
+  return renderToFile(
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>
+          <Text>{rawRecipe.title}</Text>
+          <FormattedRecipe
+            ingredients={ingredients}
+            instructions={instructions}
+          />
+        </View>
+      </Page>
+    </Document>,
+    `${process.cwd()}/my-doc.pdf`
+  );
+};
 
-}
+generatePDF({
+  title: "Mushroom Chicken",
+  ingredients:
+    "400g chicken breast\n100g mushrooms\n20cL full cream\n2 spoons Herbes de Provence\n2 onions\n2 tbsp Paprika",
+  instructions:
+    "Cook the onions\nAdd the mushrooms\nAdd the herbs\nGrill the chicken separately\nYou're done!",
+});
